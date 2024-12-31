37cd904860282c5e2702a042b460c80648427f52
yaozhixiang <yaozhixiang@taqu.cn>
2024-12-31 09:23:22 +0800

    feat: 改进 useModalTranslateDraggable 方法

--- src/content/composables/useModalTranslateDraggable.ts
+++ src/content/composables/useModalTranslateDraggable.ts
@@ -1,4 +1,67 @@
-import { useDraggable, useElementSize, useWindowSize } from '@vueuse/core'
+import { useDraggable, useElementBounding, useElementSize, useWindowSize } from '@vueuse/core'
+
+interface ParamsDraggable {
+  containerTop: Ref<number>
+  containerLeft: Ref<number>
+  sizeContainer: ReturnType<typeof useElementSize>
+  distance: number
+}
+
+function useWindowDraggable(params: ParamsDraggable) {
+  const sizeWindow = useWindowSize()
+  const top = computed<number>(() => {
+    const maxHeight = sizeWindow.height.value - params.sizeContainer.height.value - params.distance
+
+    if (params.containerTop.value < 0)
+      return 0 + params.distance
+
+    if (params.containerTop.value <= maxHeight)
+      return params.containerTop.value
+
+    return maxHeight
+  })
+  const left = computed<number>(() => {
+    const maxWidth = sizeWindow.width.value - params.sizeContainer.width.value - params.distance
+
+    if (params.containerLeft.value < 0)
+      return 0 + params.distance
+
+    if (params.containerLeft.value <= maxWidth)
+      return params.containerLeft.value
+
+    return maxWidth
+  })
+
+  return { top, left }
+}
+
+function useRootDraggable(params: ParamsDraggable & { root: HTMLElement }) {
+  const sizeRoot = useElementBounding(params.root)
+  const top = computed<number>(() => {
+    const maxHeight = sizeRoot.height.value + sizeRoot.top.value - params.sizeContainer.height.value - params.distance
+
+    if (params.containerTop.value < sizeRoot.top.value)
+      return sizeRoot.top.value + params.distance
+
+    if (params.containerTop.value <= maxHeight)
+      return params.containerTop.value
+
+    return maxHeight
+  })
+  const left = computed<number>(() => {
+    const maxWidth = sizeRoot.width.value + sizeRoot.left.value - params.sizeContainer.width.value - params.distance
+
+    if (params.containerLeft.value < sizeRoot.left.value)
+      return sizeRoot.left.value + params.distance
+
+    if (params.containerLeft.value <= maxWidth)
+      return params.containerLeft.value
+
+    return maxWidth
+  })
+
+  return { top, left }
+}
 
 interface Params {
   x: Ref<number>
@@ -12,36 +75,18 @@ export function useModalTranslateDraggable(params: Params) {
 
   const refHeader = ref<HTMLElement | null>(null)
   const refContainer = ref<HTMLElement | null>(null)
-
-  const { position, isDragging } = useDraggable(refHeader)
-  const sizeRoot = root ? useElementSize(root) : useWindowSize() // TODO
   const sizeContainer = useElementSize(refContainer)
-  const top = computed<number>(() => {
-    const containerTop = position.value.y || y.value
-
-    const maxHeight = sizeRoot.height.value - sizeContainer.height.value
-
-    if (containerTop < 0)
-      return 0
-
-    if (containerTop <= maxHeight)
-      return containerTop
-
-    return maxHeight
-  })
-  const left = computed<number>(() => {
-    const containerLeft = position.value.x || x.value
-
-    const maxWidth = sizeRoot.width.value - sizeContainer.width.value
-
-    if (containerLeft < 0)
-      return 0
-
-    if (containerLeft <= maxWidth)
-      return containerLeft
-
-    return maxWidth
-  })
+  const { position, isDragging } = useDraggable(refHeader)
+  const containerTop = computed(() => position.value.y || y.value)
+  const containerLeft = computed(() => position.value.x || x.value)
+  const basicParams = {
+    containerTop,
+    containerLeft,
+    sizeContainer,
+  }
+  const { top, left } = root
+    ? useRootDraggable({ ...basicParams, distance: 1, root })
+    : useWindowDraggable({ ...basicParams, distance: 0 })
 
   watch(open, (open) => {
     if (open === false) {
