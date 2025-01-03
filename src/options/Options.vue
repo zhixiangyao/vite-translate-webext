1539d7b050ab8b85182e186ad859bb32c1fb6a67
zhixiangyao <blueskyzx@icloud.com>
2025-01-03 14:48:18 +0800

    feat: Options - 抽象一个 AntProvider 高阶组件

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -1,32 +1,11 @@
 <script setup lang="ts">
-import type { ConfigProviderProps } from 'ant-design-vue'
-import type { SeedToken } from 'ant-design-vue/es/theme/internal'
-import { useDark } from '@vueuse/core'
-import { App, ConfigProvider, theme } from 'ant-design-vue'
-
-const tokenDark = theme.darkAlgorithm({
-  ...theme.defaultSeed,
-  borderRadius: 2,
-  colorPrimary: '#A0D911',
-} satisfies SeedToken)
-const tokenLight = theme.defaultAlgorithm({
-  ...theme.defaultSeed,
-  borderRadius: 2,
-  colorPrimary: '#A0D911',
-} satisfies SeedToken)
-
-const isDark = useDark()
-const defaultTheme = computed<ConfigProviderProps['theme']>(() => ({
-  token: isDark.value ? tokenDark : tokenLight,
-}))
+import AntProvider from '~/options/components/AntProvider.vue'
 </script>
 
 <template>
-  <ConfigProvider :theme="defaultTheme">
-    <App>
-      <router-view />
-    </App>
-  </ConfigProvider>
+  <AntProvider>
+    <router-view />
+  </AntProvider>
 </template>
 
 <style>
