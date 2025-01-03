d6c15cc47b528ea80be2fc1b283465cc441062bc
zhixiangyao <blueskyzx@icloud.com>
2025-01-03 13:48:39 +0800

    fix: 路径错误

--- src/background/registers/event-activity.ts
+++ src/background/registers/event-activity.ts
@@ -1,6 +1,18 @@
 import { onMessage } from 'webext-bridge/background'
 import { storageCurrentTab } from '~/logic/storage'
 
+const actionPath = {
+  16: browser.runtime.getURL(`assets/icons/icon-16.png`),
+  48: browser.runtime.getURL(`assets/icons/icon-48.png`),
+  128: browser.runtime.getURL(`assets/icons/icon-128.png`),
+}
+
+const unActionPath = {
+  16: browser.runtime.getURL(`assets/icons/icon-gray-16.png`),
+  48: browser.runtime.getURL(`assets/icons/icon-gray-48.png`),
+  128: browser.runtime.getURL(`assets/icons/icon-gray-128.png`),
+}
+
 onMessage('event-activity', async ({ data }) => {
   const tabId = storageCurrentTab.value.id
 
@@ -8,23 +20,9 @@ onMessage('event-activity', async ({ data }) => {
     return
 
   if (data.show) {
-    browser.action.setIcon({
-      tabId,
-      path: {
-        16: browser.runtime.getURL(`assets/icons/icon-16.png`),
-        48: browser.runtime.getURL(`assets/icons/icon-48.png`),
-        128: browser.runtime.getURL(`assets/icons/icon-128.png`),
-      },
-    })
+    browser.action.setIcon({ tabId, path: actionPath })
   }
   else {
-    browser.action.setIcon({
-      tabId,
-      path: {
-        16: browser.runtime.getURL(`assets/icons/icon-gray-16.png`),
-        48: browser.runtime.getURL(`assets/icons/icon-gray-48.png`),
-        128: browser.runtime.getURL(`assets/icons/icon-gray-128.png`),
-      },
-    })
+    browser.action.setIcon({ tabId, path: unActionPath })
   }
 })
