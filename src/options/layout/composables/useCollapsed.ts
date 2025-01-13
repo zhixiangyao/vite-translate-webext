0f9b092cba3419c8973557c7d2ecd72c1ffbd92c
zhixiangyao <blueskyzx@icloud.com>
2025-01-13 20:19:44 +0800

    feat: Options - 改进 useCollapsed 逻辑, 当 window 的 width 小于 1024 自动 collapsed

--- src/options/layout/composables/useCollapsed.ts
+++ src/options/layout/composables/useCollapsed.ts
@@ -1,9 +1,11 @@
-import { useUrlSearchParams } from '@vueuse/core'
+import { refDebounced, useUrlSearchParams, useWindowSize } from '@vueuse/core'
 
 type Collapsed = 'true' | 'false'
 
 export function useCollapsed() {
   const params = useUrlSearchParams<{ collapsed: Collapsed }>('hash', { initialValue: { collapsed: 'false' } })
+  const size = useWindowSize()
+  const debouncedWidth = refDebounced(size.width, 500)
   const collapsed = computed({
     get() {
       return params.collapsed === 'true'
@@ -12,6 +14,8 @@ export function useCollapsed() {
       params.collapsed = bool ? 'true' : 'false'
     },
   })
+
+  watch(debouncedWidth, width => (collapsed.value = width <= 1024))
 
   return collapsed
 }
