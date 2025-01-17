b40f79681b6ba6fe6ed4980e2672ee762d7479b0
zhixiangyao <blueskyzx@icloud.com>
2025-01-17 22:07:29 +0800

    feat: Options - 使用 useStorage 替代 useUrlSearchParams

--- src/options/layout/composables/useCollapsed.ts
+++ src/options/layout/composables/useCollapsed.ts
@@ -1,19 +1,9 @@
-import { refDebounced, useUrlSearchParams, useWindowSize } from '@vueuse/core'
-
-type Collapsed = 'true' | 'false'
+import { refDebounced, useStorage, useWindowSize } from '@vueuse/core'
 
 export function useCollapsed() {
-  const params = useUrlSearchParams<{ collapsed: Collapsed }>('hash', { initialValue: { collapsed: 'false' } })
+  const collapsed = useStorage<boolean>('collapsed', false)
   const size = useWindowSize()
   const debouncedWidth = refDebounced(size.width, 500)
-  const collapsed = computed({
-    get() {
-      return params.collapsed === 'true'
-    },
-    set(bool) {
-      params.collapsed = bool ? 'true' : 'false'
-    },
-  })
 
   watch(debouncedWidth, width => (collapsed.value = width <= 1024))
 
